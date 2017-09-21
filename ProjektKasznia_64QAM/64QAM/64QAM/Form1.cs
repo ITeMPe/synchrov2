a49dbf072a12e4ff663134e5046eaefb4dca007a
kkarp <kkarp@KAMIL-DESKTOP>
2017-09-21 21:09:26 +0100

    First COmit

---
+++ ProjektKasznia_64QAM/64QAM/64QAM/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _64QAM
+{
+    public partial class Form1 : Form
+    {
+        private int ograniczenie = 0;
+        private double lastChange = 0;
+        private float procentSzerokosciOkna_ { get; set; } = 0.4f;
+        public int ProcentSzerokosciOkna { get; private set; }
+        List<MyComplex> ListaPunktowKonstekacji = new List<MyComplex>();
+        List<MyComplex> ListaPunktowKonstekacji2 = new List<MyComplex>();
+        public Form1()
+        {
+            InitializeComponent();
+            ListaPunktowKonstekacji = StworzPunktyKonstelacji();
+            WstawKonstelacje(chart1, ListaPunktowKonstekacji);
+
+            SetupChart(chart1);
+            SetupChart(chart2);
+
+
+        }
+
+        public void SetupChart(Chart chart)
+        {
+            #region Grid
+            chart.ChartAreas[0].AxisX.Maximum = 11;
+            chart.ChartAreas[0].AxisX.Minimum = -11;
+            chart.ChartAreas[0].AxisY.Maximum = 11;
+            chart.ChartAreas[0].AxisY.Minimum = -11;
+
+            chart.ChartAreas[0].AxisX.Interval = 2;
+            chart.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Black;
+
+            chart.ChartAreas[0].AxisY.Interval = 2;
+            chart.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Black;
+            #endregion
+
+            #region Dodanie_Poczatku_Ukladow_wspolrzednych
+            StripLine striplineY = new StripLine();
+            striplineY.Interval = 0;
+            striplineY.IntervalOffset = -0.05;
+            striplineY.StripWidth = 0.1;
+            striplineY.BackColor = Color.Black;
+            chart.ChartAreas[0].AxisY.StripLines.Add(striplineY);
+            StripLine striplineX = new StripLine();
+            striplineX.Interval = 0;
+            striplineX.IntervalOffset = -0.05;
+            striplineX.StripWidth = 0.1;
+            striplineX.BackColor = Color.Black;
+            chart.ChartAreas[0].AxisX.StripLines.Add(striplineX);
+            #endregion
+        }
+
+        public List<MyComplex> StworzPunktyKonstelacji()
+        {
+            var list = new List<MyComplex>();
+            for (int i = -7; i <= 7; i = i + 2)
+            {
+                for (int j = -7; j <= 7; j = j + 2)
+                {
+                    list.Add(new MyComplex(i, j));
+                }
+            }
+            return list;
+        }
+
+        public void WstawKonstelacje(Chart chart, List<MyComplex> konstelacja)
+        {
+            chart.Series.Clear();
+            var constelationSeries = new Series()
+            {
+                ChartType = SeriesChartType.Point,
+                MarkerSize = 7,
+                MarkerStyle = MarkerStyle.Circle
+            };
+
+            foreach (var s in konstelacja)
+            {
+                constelationSeries.Points.AddXY(s.Real, s.Imagine);
+            }
+            chart.Series.Add(constelationSeries);
+         
+        }
+
+        public void dodajKonstelacje(Chart chart, List<MyComplex> konstelacja)
+        {
+            if (ograniczenie++ > 5)
+            {
+                chart.Series.Clear();
+                ograniczenie = 0;
+            }
+            var constelationSeries = new Series()
+            {
+                ChartType = SeriesChartType.Point,
+                MarkerSize = 7,
+                MarkerStyle = MarkerStyle.Circle,
+                MarkerColor = Color.Black 
+            };
+
+            foreach (var s in konstelacja)
+            {
+                constelationSeries.Points.AddXY(s.Real, s.Imagine);
+            }
+            chart.Series.Add(constelationSeries);
+        }
+
+
+        public void ZmienKolor(Chart chart, double x, double y, Color color)
+        {
+            var foo = chart.Series.Select(s => s.Points.Where(r => r.XValue == x && r.YValues[0] == y).FirstOrDefault()).FirstOrDefault();
+            foo.Color = color;
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = trackBar1.Value.ToString() + "°";
+            double phase = trackBar1.Value * Math.PI / 180;
+            if (checkBoxFaza.Checked)
+            {
+                wplywFazyNaKOnstelacje(phase);
+            }
+        }
+
+        private void wplywFazyNaKOnstelacje(double phase)
+        {
+            List<MyComplex> result = new List<MyComplex>();
+
+
+            if (lastChange < phase)
+            {
+
+                foreach (var s in ListaPunktowKonstekacji)
+                {
+                    result.Add(new MyComplex
+                    {
+                        Imagine = s.Imagine,
+                        Real = s.Real,
+                        Phase = s.Phase + phase
+                    }
+                   );
+
+                }
+            }
+            else
+            {
+                foreach (var s in ListaPunktowKonstekacji)
+                {
+                    result.Add(new MyComplex
+                    {
+                        Imagine = s.Imagine,
+                        Real = s.Real,
+                        Phase = s.Phase - phase
+                    }
+                   );
+
+                }
+            }
+            WstawKonstelacje(chart2, result);
+            lastChange = phase;
+        }
+
+        private void trackBarCzestotliwosc_ValueChanged(object sender, EventArgs e)
+        {
+            label2.Text = trackBarCzestotliwosc.Value.ToString() + "Hz";
+            
+            List<MyComplex> result = new List<MyComplex>();
+            for (double t = 0; t < (double)numericUpDown1.Value*5; t += (double)numericUpDown1.Value)
+            {
+
+
+
+                foreach (var s in ListaPunktowKonstekacji)
+                {
+                    result.Add(new MyComplex
+                    {
+                        Imagine = s.Imagine,
+                        Real = s.Real,
+                        Phase = s.Phase + trackBarCzestotliwosc.Value * t
+                    }
+                   );
+                   
+                }
+                    dodajKonstelacje(chart2, result);
+            }
+           
+        }
+    }
+}
---
+++ ProjektKasznia_64QAM/64QAM/64QAM/MyComplex.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _64QAM
+{
+    public class MyComplex
+    {
+        public MyComplex()
+        {
+
+        }
+        public MyComplex(double real, double imagine)
+        {
+            Real_ = real;
+            Imagine_ = imagine;
+            Phase_ = Math.Atan(Math.Abs(Imagine_) / Math.Abs(Real_));
+            Madnitude_ = Math.Sqrt(Math.Pow(Real_, 2) + Math.Pow(Imagine_, 2));
+            if (Real_ < 0 && Imagine_ > 0) //2
+            {
+                Phase_ += Math.PI / 2;
+            }
+            if (Real_ < 0 && Imagine_ < 0)  //3
+            {
+                Phase_ += Math.PI;
+            }
+            if (Real_ > 0 && Imagine_ < 0) //4
+            {
+                Phase_ += Math.PI * 1.5;
+            }
+        }
+
+        private double Real_;
+        private double Imagine_;
+        private double Phase_;
+        private double Madnitude_;
+
+        public double Real
+        {
+            get
+            {
+                return Real_;
+            }
+            set
+            {
+                Real_ = value;
+                var faza1 = Math.Atan(Imagine_ / Real_);
+                Madnitude_ = Math.Sqrt(Math.Pow(Real_, 2) + Math.Pow(Imagine_, 2));
+                if (value > 0)   //1 4
+                {
+                    Phase_ = faza1;
+                }
+                else  // 2 3
+                {
+                    Phase_ = Math.PI + faza1;
+                }
+            }
+        }
+        public double Imagine
+        {
+            get
+            {
+                return Imagine_;
+            }
+            set
+            {
+                Imagine_ = value;
+                var faza1 = Math.Atan(Imagine_ / Real_);
+                Madnitude_ = Math.Sqrt(Math.Pow(Real_, 2) + Math.Pow(Imagine_, 2));
+                if (Real_ > 0)   //1 4
+                {
+                    Phase_ = faza1;
+                }
+                else  // 2 3
+                {
+                    Phase_ = Math.PI + faza1;
+                }
+            }
+        }
+        public double Phase
+        {
+            get
+            {
+                return Phase_;
+            }
+            set
+            {
+
+                Phase_ = value;
+                Imagine_ = Madnitude_ * Math.Sin(Phase_);
+                Real_ = Madnitude_ * Math.Cos(Phase_);
+
+
+                //if (Phase_ > Math.PI / 2 && Phase_ < Math.PI)  //2
+                //{
+
+                //    Real_ *= -1;
+                //}
+                //if (Phase_ > Math.PI && Phase_ < Math.PI * 1.5)  //3
+                //{
+                //    Imagine_ *= -1;
+                //    Real_ *= -1;
+                //}
+                //if (Phase_ > Math.PI * 1.5 && Phase_ < Math.PI * 2) //4
+                //{
+                //    Imagine_ *= -1;
+
+                //}
+
+                //Phase_ = value;  
+                //if(Phase_ > 0 && Phase_ < Math.PI/2)    //1
+                //{
+                //Imagine_ = Madnitude_ * Math.Sin(Phase_);
+                //Real_ = Madnitude_ * Math.Cos(Phase_);  
+                //}
+                //if (Phase_ > Math.PI/2 && Phase_ < Math.PI )  //2
+                //{
+                //    Imagine_ = Madnitude_ * Math.Sin(Phase_- Math.PI / 2);
+                //    Real_ = Madnitude_ * Math.Cos(Phase_- Math.PI / 2) * -1;
+                //}
+                //if (Phase_ > Math.PI && Phase_ < Math.PI * 1.5)  //3
+                //{
+                //    Imagine_ = Madnitude_ * Math.Sin(Phase_ - Math.PI) * -1;
+                //    Real_ = Madnitude_ * Math.Cos(Phase_ - Math.PI) * -1;
+                //}
+                //if (Phase_ > Math.PI * 1.5 && Phase_ < Math.PI * 2) //4
+                //{
+                //    Imagine_ = Madnitude_ * Math.Sin(Phase_ - Math.PI * 1.5) *-1;
+                //    Real_ = Madnitude_ * Math.Cos(Phase_ - Math.PI * 1.5);
+                //}    
+            }
+        }
+    }
+}
